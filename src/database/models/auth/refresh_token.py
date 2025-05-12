92c16730f3bc9eea11467d41708f53f89bee2088
Mariya Tkacheva <masha.tk02@gmail.com>
2025-05-12 03:39:53 +0300

    refresh tokens (oops i forgot)

---
+++ src/database/models/auth/refresh_token.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field, model_validator, ConfigDict
+
+from ..utils import PyObjectId
+
+
+class RefreshTokenDB(BaseModel):
+    id: Optional[PyObjectId] = Field(default_factory=PyObjectId, alias="_id")
+    user_id: int = Field(..., alias="userId")
+    token_hash: str = Field(..., alias="tokenHash")
+    expires_at: datetime
+
+    @model_validator(mode='before')
+    def parse_date(cls, values):
+        date_value = values.get("date")
+        if isinstance(date_value, dict) and "$date" in date_value:
+            date_value = date_value["$date"]
+        if isinstance(date_value, str):
+            values["date"] = datetime.fromisoformat(date_value.replace("Z", "+00:00"))
+        return values
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={"collection": "eventBankingAccounts"},
+    )
---
+++ src/database/repositories/auth/refresh_token_repository.py
@@ -0,0 +1,19 @@
+from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorClientSession
+
+from src.utils.simple_result import SimpleResult, SimpleErrorResult, SimpleOkResult
+from ..async_base_repository import AsyncRepository
+from ...models.auth.refresh_token import RefreshTokenDB
+
+
+class RefreshTokensRepository(AsyncRepository[RefreshTokenDB]):
+    def __init__(self, db: AsyncIOMotorDatabase):
+        super().__init__(db, RefreshTokenDB, "refreshTokens")
+
+    async def find_one_by_user(self, user_id: int, session: AsyncIOMotorClientSession | None = None) -> SimpleResult[RefreshTokenDB]:
+        try:
+            doc = await self.collection().find_one({"userId": user_id}, session=session)
+            if not doc:
+                return SimpleErrorResult(f"Document with id={object_id} not found")
+            return SimpleOkResult(payload=self._model(**doc))
+        except Exception as e:
+            return SimpleErrorResult(str(e))
