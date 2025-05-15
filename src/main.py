5b004e573c42144aae88caa54b3ad2244058656e
Mariya Tkacheva <masha.tk02@gmail.com>
2025-05-15 18:10:31 +0300

    main / cleaned

--- src/main.py
+++ src/main.py
@@ -22,27 +22,4 @@ app.include_router(teacher_router)
 # app.include_router(admin_router)
 @app.get("/")
 async def read_root():
-    repo = PeopleRepository(db=db)
-    try:
-        await add_fake_users()
-        print(await get_all_persons(repo))
-        print('success')
-        return JSONResponse(status_code=200, content={})
-    except ValidationError as e:
-        print('error')
-        print(e.errors())
-        return JSONResponse(status_code=400,content={})
-    except Exception as e:
-        print('not validation error: {}'.format(e))
-        return JSONResponse(status_code=400,content={})
-
-
-async def get_all_persons(repo) -> List[PersonDB]:
-    # Используем асинхронный запрос для получения всех записей
-    res = await repo.find_all()
-    print(res.__class__)
-    if isinstance(res, SimpleOkResult):
-        return res.payload
-    else:
-        print(res.message)
-        raise Exception(res.message)
+    return JSONResponse(status_code=200,content={})
